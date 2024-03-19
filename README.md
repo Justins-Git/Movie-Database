a5413cd192c7315a70baf7fda6d6ddb044b90b62
Ryan Symons <47405201+rsymons22@users.noreply.github.com>
2024-03-19 12:58:49 -0400

    Python database connection

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ SQL database with Java application
 JDBC postgresql driver from: https://jdbc.postgresql.org/download/ (java 8+)
 
 RIT Java postgresql example: https://wiki.cs.rit.edu/index.php/Database_Server
+
+How to get python database connection running:
+
+1) Download python/pip
+
+2) pip install sshtunnel
+
+3) pip install psycopg2
+
+4) put login information in logininfo.txt
+
+5) run application.py, "Database connection established" should print to terminal
---
+++ src/application.py
@@ -0,0 +1,35 @@
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+file = open("src/logininfo.txt", "r")
+username = file.readline().strip()
+password = file.readline().strip()
+file.close()
+
+dbName = "p320_18"
+
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('127.0.0.1', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+
+        conn = psycopg2.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+
+        #DB work here....
+
+        conn.close()
+except:
+    print("Connection Failed...")
