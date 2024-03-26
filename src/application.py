2b1b6d9840885c758a8e243cd5bb3698d5dc69ba
Ryan Symons <47405201+rsymons22@users.noreply.github.com>
2024-03-25 21:46:37 -0400

    Friend functionality added

--- src/application.py
+++ src/application.py
@@ -35,17 +35,18 @@ def main():
             conn = psycopg2.connect(**params)
             curs = conn.cursor()
             print("Database connection established")
-            
+
             username = login(conn, curs)
             # If the user entered valid login information or created an account
             if username != None:  
                 while True:
+                    print("---------------------------------------------------------------------")
                     result = input("View Collections (C) | View Movies (M) | View Friends (F) | Quit (Q): ")
 
-                    if(result == "C"): collections(conn, curs, username)
-                    elif(result == "M"): movies(conn, curs, username)
-                    elif(result == "F"): friends(conn, curs, username)
-                    elif(result == "Q"): break
+                    if result == "C": collections(conn, curs, username)
+                    elif result == "M": movies(conn, curs, username)
+                    elif result == "F": friends(conn, curs, username)
+                    elif result == "Q": break
 
                             
                         
@@ -61,7 +62,41 @@ def movies(conn, curs, username):
     return
 
 def friends(conn, curs, username):
-    return
+    curs.execute("SELECT username_friend FROM user_friend WHERE username = %s;", (username,))
+    for friend in curs:
+        print(f"| {friend[0]}")
+
+    while True:
+        result = input("Remove Friend (R friend-username) | Add Friend (A friend-email) | Quit (Q): ")
+
+        if result[0:1] == "R":
+            friend = result[2:]
+
+            curs.execute("SELECT FROM user_friend WHERE username=%s AND username_friend=%s", (username, friend,))
+            friend_exists = curs.fetchone() != None
+
+            if friend_exists:
+                curs.execute("DELETE FROM user_friend WHERE username=%s AND username_friend=%s", (username, friend,))
+                conn.commit()
+                print(f"> {friend} removed from friends list successfully")
+            else:
+                print(f"> User {friend} could not be found")
+
+        elif result[0:1] == "A":
+            friend_email = result[2:]
+            curs.execute("SELECT username FROM user_email where email=%s;", (friend_email,))
+            friend_username = curs.fetchone()
+            
+            if friend_username != None:
+                friend_username = friend_username[0]
+                curs.execute("INSERT INTO user_friend (username, username_friend) VALUES (%s, %s);", (username, friend_username,))
+                conn.commit()
+
+                print(f"> User {friend_username} with email {friend_email} has been added")
+            else:
+                print(f"> User with email {friend_email} could not be found")
+
+        elif result[0:1] == "Q": break 
 
 def login(conn, curs):
     username = input("Enter username: ")
@@ -83,14 +118,15 @@ def login(conn, curs):
             curs.execute("INSERT INTO movie_user (username, password, creation_date, last_access_date, first_name, last_name) VALUES (%s, %s, %s, %s, %s, %s);", (username, password, datetime.date.today(), datetime.date.today(), firstname, lastname))
             conn.commit()
             print("Account successfully created")
+    else:
+        print("> Login Successful")
     
-    # If the user already accessed the application today, a unique violation would be thrown
-    try:
+    curs.execute("SELECT * FROM user_access_date where access_date=%s AND username=%s", (datetime.date.today(), username,))
+
+    if curs.fetchone() == None:
         curs.execute("INSERT INTO user_access_date (access_date, username) VALUES (%s, %s);", (datetime.date.today(), username))
         conn.commit()
-    except:
-        pass
-        
+
     return username
 
 
